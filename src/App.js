6c54cab17d81d3948bca85b19152767980c945fe
f0dder1024 <mlee1024@gmail.com>
2020-03-22 23:27:31 -0700

    Wired up actions and reducers to fetch posts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import Header from './component/header/Header';
 import Headline from './component/headline/Headline';
+import SharedButton from './component/button/SharedButton';
+import ListItem from './component/listItem/ListItem';
+import {connect} from 'react-redux';
+import fetchPostsAction from './actions/fetchPostsAction';
 import './app.scss';
 
-const tempArr = [
-  {
-    fName: 'Joe',
-    lName: 'Blogs',
-    email: 'jblogs@email.com',
-    age: 24,
-    onlineStatus: true
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.fetch = this.fetch.bind(this);
   }
-]
 
+  fetch(){
+    this.props.fetchPostsAction();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <section className="main">
-        <Headline header="Posts" desc="Click the button to render posts!" tempArr={tempArr}/>
-      </section>
-    </div>
-  );
+  render(){
+    const { posts } = this.props;
+    const configButton = {
+      buttonText: 'Get posts',
+      emitEvent: this.fetch
+    }
+    return (
+      <div className="App">
+        <Header></Header>
+        <section className="main">
+          <Headline header="Posts" desc="Click the button to render posts!" />
+          <SharedButton {...configButton}/>
+          {posts.length > 0 &&
+            <div>
+              {posts.map((post)=>{
+                const {title, body} = post;
+                const configListItem = {
+                  title,
+                  description: body
+                }
+                return (<ListItem  key={title} {...configListItem}/>)
+              })}
+            </div>
+          }
+        </section>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    posts: state.postsReducer
+  }
+}
+
+export default connect(mapStateToProps, {fetchPostsAction})(App);
